2aeee04c92987cdc2f7ed1ddc575fec1326ecabd
Alexander <evening.sasha@mail.ru>
2022-01-28 22:54:52 +0300

    JSX

---
+++ 01.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+
+const App = () => {
+  return (
+      <div>
+      <div> <Header /> </div>
+    <div className="App">
+      <ul>
+          <li>css</li>
+          <li>html</li>
+          <li>js</li>
+          <li>react</li>
+      </ul>
+
+    </div>
+  </div>
+);
+}
+const Header = () => {
+    return (<div>
+        <a href='#s'>Home</a>
+        <a href='#s'>News Feed</a>
+        <a href='#s'>Messages</a>
+    </div>)
+}
+
+export default App;
