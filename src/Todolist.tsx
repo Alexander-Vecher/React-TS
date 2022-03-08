5920e16542c977c7aad106806c61ac08689d600d
alexander-vecher <evening.sasha@mail.ru>
2022-03-08 23:23:14 +0300

    x

--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -31,11 +31,6 @@ export let Todolist = (props: TodolistPropsType) => {
                         <input type="checkbox" checked={el.isDone}/> <span>{el.title}</span></li>
                 )
             })}
-            {/*<ul>
-                <li><input type="checkbox" checked={props.task[0].isDone}/> <span>{props.task[0].title}</span></li>
-                <li><input type="checkbox" checked={props.task[1].isDone}/> <span>{props.task[1].title}</span></li>
-                <li><input type="checkbox" checked={props.task[2].isDone}/> <span>{props.task[2].title}</span></li>
-            </ul>*/}
             <div>
                 <button onClick={()=>props.Value('All')}>All</button>
                 <button onClick={()=>props.Value('Active')}>Active</button>
